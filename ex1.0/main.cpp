6bfde099755a7a3855b3133c6d1061241b97e9cb
Hamza Khan Niazi <Hamza Khan Niazi@130.83.104.146>
2015-09-17 11:24:08 +0200

    asfd

---
+++ ex1.0/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 17.09.2015
+ *      Author: Hamza Khan Niazi
+ */
+#include <iostream>
+int main() {
+	std::cout << "Hello World" << std::endl; // prints "Hello World"
+}
+
